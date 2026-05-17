80c4c3efdbe99662774844c911e7a49105776dc9
princekumar16630-commits <princekumar16630@gmail.com>
2026-05-17 09:47:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const mineflayer = require("mineflayer");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+let deadBot;
+let princeBot;
+
+function createBot(name) {
+  const bot = mineflayer.createBot({
+    host: "karmasmp.ddns.net",
+    port: 25565,
+    username: name
+  });
+
+  bot.on("login", () => {
+    console.log(name + " joined!");
+  });
+
+  bot.on("end", () => {
+    console.log(name + " disconnected!");
+
+    setTimeout(() => {
+      createBot(name);
+    }, 5000);
+  });
+
+  setInterval(() => {
+    bot.setControlState("jump", true);
+
+    setTimeout(() => {
+      bot.setControlState("jump", false);
+    }, 500);
+  }, 30000);
+
+  return bot;
+}
+
+deadBot = createBot("Deadmau5");
+princeBot = createBot("Prince");
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Minecraft Bot Control</h1>
+
+    <form action="/send" method="POST">
+      <input name="bot" placeholder="Deadmau5 or Prince">
+      <input name="msg" placeholder="Minecraft command">
+      <button type="submit">Send</button>
+    </form>
+
+    <p>Examples:</p>
+    <p>/home farm</p>
+    <p>/msg player hi</p>
+    <p>/tp x y z</p>
+  `);
+});
+
+app.post("/send", (req, res) => {
+  const botName = req.body.bot;
+  const msg = req.body.msg;
+
+  if (botName === "Deadmau5") {
+    deadBot.chat(msg);
+  }
+
+  if (botName === "Prince") {
+    princeBot.chat(msg);
+  }
+
+  res.redirect("/");
+});
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Control panel running");
+});
